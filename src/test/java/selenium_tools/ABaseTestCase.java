c1b3a635799463da083cab0c68dcb6f564cf3c0e
lleirgarciaboadapruebas <lleirgarciaboadapruebas@gmail.com>
2017-07-18 15:35:09 +0200

    Update ABaseTestCase.java

--- src/test/java/selenium_tools/ABaseTestCase.java
+++ src/test/java/selenium_tools/ABaseTestCase.java
@@ -181,7 +181,7 @@ public abstract class ABaseTestCase{
     	}
     	wait = new WebDriverWait(driver, TIMEOUT_SECONDS);
     	System.out.println("http://192.168.204.16/tallerlavi"); 
-    	driver.get(URL);
+    	driver.get("http://192.168.204.16/tallerlavi");
     	
     	System.setProperty(SELENIUM_SCREENSHOTS, "C:/Users/Lleir Garcia/git/proyectofinal-sqa2/resource/screenshots");
     }
